a9077eb47c8501d0074beb3b8d038472a9e6ef10
50-Course <erioluwaadeodu@my.uopeople.edu>
2025-05-11 21:42:00 +0100

    set target metadata to `Base.metadata` ref

--- medline/alembic/env.py
+++ medline/alembic/env.py
@@ -7,13 +7,15 @@ from sqlalchemy import engine_from_config, pool
 from alembic import context
 from src.core.config import settings as app_settings
 
-load_dotenv()
+# from sqlmodel import SQLModel
+from src.models import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
 
-DATABASE_URL = os.getenv("DATABASE_URL") or app_settings.DATABASE_URL
+
+DATABASE_URL = app_settings.DATABASE_URL
 config.set_main_option("sqlalchemy.url", DATABASE_URL)
 
 # Interpret the config file for Python logging.
@@ -25,7 +27,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
