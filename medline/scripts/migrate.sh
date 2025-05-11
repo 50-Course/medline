c01475c13278c6a63aefb6de93b788ff66ccfcc1
50-Course <erioluwaadeodu@my.uopeople.edu>
2025-05-11 23:21:15 +0100

    feat: update db migration script with enforced migrations check rules

--- medline/scripts/migrate.sh
+++ medline/scripts/migrate.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-# generate migrations
-alembic revision --autogenerate -m 'Initial Migration'
-# apply the migration
+set -euo pipefail
+
+if [ -z $'(ls -A migrations/versions/)']; then
+	echo "No migrations file were found. Do you forget to commit them?"
+	exit 1
+fi
+
+echo '[ALEMBIC|ENTRYPOINT]: Applying database migrations...'
 alembic upgrade head
+echo '[ALEMBIC|ENTRYPOINT]: Database migrations completed.'
